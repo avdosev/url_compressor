d8f26c888e77a33b22bd783e8a8d82a392dc5c43
avdosev <avdosev1@yandex.ru>
2020-04-22 12:51:14 +0400

    разделил код на независимые части не потеряв в производительности

--- src/main.cpp
+++ src/main.cpp
@@ -5,89 +5,128 @@
 #include <sstream>
 #include <unordered_map>
 #include <map>
+#include <array>
+#include <queue>
 
+namespace fs = std::filesystem;
 
 struct hash_path {
-    std::size_t operator()(const std::filesystem::path &path) const {
+    std::size_t operator()(const fs::path &path) const {
         return hash_value(path);
     }
 };
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    using namespace std::string_literals;
-    namespace fs = std::filesystem;
 
-    fs::path name = "./data/url_all.txt";
-    fs::path data_name = "./urls.txt";
-    using std::ifstream;
-    ifstream file;
-    file.open(name);
-    if (!file.is_open()) {
-        std::cout << "file " << name << " not open" << std::endl;
-        return -1;
+size_t raw_line_counter(std::istream& istream) {
+    size_t res = 0;
+    constexpr size_t buff_size = 1024*64;
+    std::vector<char> buff(buff_size);
+    while (!istream.eof()) {
+        istream.read(buff.data(), buff.size());
+        auto const len_read = istream.gcount();
+        for (size_t i = 0; i < len_read; i++) {
+            res += buff[i] == '\n';
+        }
     }
+    return res;
+}
 
+
+template <class elapsedFnc, class ...args_t>
+void check_elapsed(elapsedFnc fnc, args_t... args) {
     auto begin = std::chrono::steady_clock::now();
 
-    auto len_http = ("http://"s).size();
+    fnc(args...);
 
-    std::unordered_map<std::filesystem::path, std::ofstream, hash_path> files;
-    //count_lines = rawgen_count(name)
-    //print('lines in file:', count_lines)
-    //percent, prev_percent = 0, 0
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "the time: " << elapsed_ms.count() << " ms" << std::endl;
+}
+
+
+class file_stream_map {
+    public:
+        file_stream_map() = default;
+        file_stream_map(const file_stream_map&) = delete;
+
+        std::ostream& try_get_or_insert(fs::path path, std::string_view filename) {
+            auto write_file_it = files.find(path);
+            if (write_file_it == files.end()) {
+                fs::create_directories(path);
+                auto full_file_path = path;
+                full_file_path /= filename;
+
+                std::ofstream f{full_file_path};
+
+                write_file_it = files.insert({path, move(f)}).first;
+            }
+
+            return write_file_it->second;
+        }
+
+        ~file_stream_map() {
+            for (auto& [dir, file] : files) {
+                file.close();
+            }
+        }
+
+    private:
+        using path_to_stream_map = std::unordered_map<fs::path, std::ofstream, hash_path>;
+        path_to_stream_map files;
+};
+
+
+void compression_url(std::string_view line, file_stream_map& files_map) {
+    static const char* filename = "urls.txt";
+    constexpr auto len_http = std::size("http://") - 1;
+
+    if (line.empty()) return;
+
+    std::string_view line_without_http(line);
+    line_without_http.remove_prefix(len_http);
+
+    auto i = line_without_http.find_last_of('/');
+
+
+    auto sections = line_without_http.substr(0, i);
+    auto image_url = line_without_http.substr(i+1);
+
+
+    fs::path path("./urls");
+    path /= sections;
+
+    auto& write_file = files_map.try_get_or_insert(path, filename);
+
+    write_file << image_url << std::endl;
+}
+
+
+void compress_file(fs::path filename) {
+    using std::ifstream;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "file " << filename << " not open" << std::endl;
+        return;
+    }
+
+    file_stream_map files;
     std::string line;
     line.reserve(350);
     while (std::getline(file, line)) {
-        if (line.empty()) break;
-        std::string_view line_without_http(line);
-        line_without_http.remove_prefix(len_http);
-
-        auto i = line_without_http.find_last_of('/');
-
-
-        auto sections = line_without_http.substr(0, i);
-        auto image_url = line_without_http.substr(i+1);
-
-
-        fs::path path("./urls/");
-        path += sections;
-
-        auto write_file_it = files.find(path);
-        if (write_file_it == files.end()) {
-            fs::create_directories(path);
-            auto filename = path;
-            filename += data_name;
-
-            std::ofstream f{filename};
-
-
-            write_file_it = files.insert({path, move(f)}).first;
-        }
-
-        auto& write_file = write_file_it->second;
-
-        write_file << image_url << std::endl;
-
-
-//        percent, prev_percent = i * 100 // count_lines, percent
-//
-//        if percent != prev_percent:
-//            print(f'{percent}%')
+        compression_url(line, files);
     }
+}
 
 
-    std::cout << "closing file descriptors..." << std::endl;
+int main() {
 
-    for (auto& [dir, file] : files) {
-        file.close();
-    }
+    std::ios_base::sync_with_stdio(false);
 
+    fs::path filename = "./data/url_all.txt";
 
-    auto end = std::chrono::steady_clock::now();
-
-    auto elapsed_ms = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
-    std::cout << "the time: " << elapsed_ms.count() << " s" << std::endl;
+    check_elapsed([&](){
+        compress_file(filename);
+    });
 
     return 0;
 }
